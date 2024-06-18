287fa51ba1829f831eb86c57d658e3eec69723d3
Can <63351166+fastuptime@users.noreply.github.com>
2024-06-18 14:09:32 +0300

    <3
    <3

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace softwareHome
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(557, 447);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(435, 166);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get System Information";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(40, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(463, 589);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1053, 636);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Get System Information - github.com/fastuptime";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Management; // !!!! NUGET PAKET YÖNETİCİSİNDEN MANAGEMENT YÜKLEYİNİZ !!!!!!
+
+
+namespace softwareHome
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            ListSystemInfo();
+        }
+
+        private void ListSystemInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Operating System Information:");
+            sb.AppendLine(GetManagementObject("Win32_OperatingSystem", "Caption"));
+            sb.AppendLine(GetManagementObject("Win32_OperatingSystem", "Version"));
+            sb.AppendLine(GetManagementObject("Win32_OperatingSystem", "Manufacturer"));
+            sb.AppendLine();
+
+            sb.AppendLine("Processor Information:");
+            sb.AppendLine(GetManagementObject("Win32_Processor", "Name"));
+            sb.AppendLine(GetManagementObject("Win32_Processor", "Manufacturer"));
+            sb.AppendLine(GetManagementObject("Win32_Processor", "Description"));
+            sb.AppendLine();
+
+            sb.AppendLine("Memory Information:");
+            sb.AppendLine(GetManagementObject("Win32_PhysicalMemory", "Capacity"));
+            sb.AppendLine(GetManagementObject("Win32_PhysicalMemory", "Speed"));
+            sb.AppendLine();
+
+            sb.AppendLine("Disk Drive Information:");
+            sb.AppendLine(GetManagementObject("Win32_DiskDrive", "Model"));
+            sb.AppendLine(GetManagementObject("Win32_DiskDrive", "InterfaceType"));
+            sb.AppendLine(GetManagementObject("Win32_DiskDrive", "MediaType"));
+            sb.AppendLine();
+
+            sb.AppendLine("Network Adapter Information:");
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                sb.AppendLine($"Name: {ni.Name}");
+                sb.AppendLine($"Description: {ni.Description}");
+                sb.AppendLine($"Status: {ni.OperationalStatus}");
+                sb.AppendLine($"Speed: {ni.Speed}");
+                sb.AppendLine();
+            }
+
+            richTextBox1.Text = sb.ToString();
+        }
+
+        private string GetManagementObject(string query, string property)
+        {
+            StringBuilder sb = new StringBuilder();
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher($"SELECT {property} FROM {query}");
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                sb.AppendLine($"{property}: {obj[property]}");
+            }
+            return sb.ToString();
+        }
+    }
+}
